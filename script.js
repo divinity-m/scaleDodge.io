9413aff3c03897b0f59a858775ed558508d5f1c7
Vasto <sonicdarkultra44@gmail.com>
2025-09-18 18:09:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO - SCRIPT.JS
-console.log("unscaled the canvas")
+console.log("got everything fundamentally down")
 const cnv = document.getElementById("game");
 const ctx = cnv.getContext('2d');
 
